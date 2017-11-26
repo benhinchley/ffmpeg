44a374b4f894eea721ea30ba625a53252b4a8181
Benjamin Hinchley <benjaminhinchley@gmail.com>
2017-11-26 22:10:38 +1100

    add basic usage

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # ffmpeg
 > a go library interface around the ffmpeg command line tool
 
+## Usage
+```go
+// For creating a video from many images
+cmd, err := ffmpeg.Command(nil, ffmpeg.Input("foo-%03d.jpeg", ffmpeg.WithFramerate(12)), ffmpeg.Output("foo.avi", ffmpeg.WithSize(1920,1080)))
+if err != nil {
+	// ... handle error
+}
+
+if err := cmd.Run(); err != nil {
+	// ... handle error
+}
+```
 
 ## License
 [MIT](LICENSE)
---
+++ options_video.go
@@ -0,0 +1,25 @@
+package ffmpeg
+
+import "fmt"
+
+// Video options:
+// -vframes number     set the number of video frames to output
+// -r rate             set frame rate (Hz value, fraction or abbreviation)
+// -s size             set frame size (WxH or abbreviation)
+// -aspect aspect      set aspect ratio (4:3, 16:9 or 1.3333, 1.7777)
+// -bits_per_raw_sample number  set the number of bits per raw sample
+// -vn                 disable video
+// -vcodec codec       force video codec ('copy' to copy stream)
+// -timecode hh:mm:ss[:;.]ff  set initial TimeCode value.
+// -pass n             select the pass number (1 to 3)
+// -vf filter_graph    set video filters
+// -ab bitrate         audio bitrate (please use -b:a)
+// -b bitrate          video bitrate (please use -b:v)
+// -dn                 disable data
+
+func WithSize(w, h int) FileOption {
+	return func(f *File) error {
+		f.options = append(f.options, []string{"-s", fmt.Sprintf("%dx%d", w, h)}...)
+		return nil
+	}
+}
